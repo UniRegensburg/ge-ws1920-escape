7c88f1b614f3ac857a9a380b1d2464a342156895
AndreasHennings <a3nd@gmx.de>
2020-02-25 18:07:05 +0100

    Added more stuff

--- Escape/Assets/Scripts/GameManager.cs
+++ Escape/Assets/Scripts/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
-        throw new NotImplementedException();
+        GameObject explosion = GameObject.Find("Explosion");
+        explosion.SetActive(true);
     }
 }
